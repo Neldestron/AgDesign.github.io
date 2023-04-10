9be7dac11e6b07a43c65be3bc0d767cf4b229922
Neldestron <125698450+Neldestron@users.noreply.github.com>
2023-04-10 12:19:56 -0400

    cambios

--- information/Information.js
+++ information/Information.js
@@ -29,3 +29,20 @@ let selected= document.querySelector(".btn-menu")
 selected.addEventListener("click",()=>{
     document.querySelector(".letter-navegation").classList.toggle("selected")
 })
+
+let btnInfo1=document.querySelector(".btn-info1")
+let btnInfo2=document.querySelector(".btn-info2")
+let btnInfo3=document.querySelector(".btn-info3")
+
+btnInfo1.addEventListener("click",()=>{
+    document.querySelector(".show-info1").classList.toggle("info1")
+}
+)
+btnInfo2.addEventListener("click",()=>{
+    document.querySelector(".show-info2").classList.toggle("info4")
+}
+)
+btnInfo3.addEventListener("click",()=>{
+    document.querySelector(".show-info3").classList.toggle("info3")
+}
+)
