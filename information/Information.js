a4d4d066438021974561fa8c8ec1ec882f7879cf
Neldestron <125698450+Neldestron@users.noreply.github.com>
2023-04-14 16:13:11 -0400

    cambios

--- information/Information.js
+++ information/Information.js
@@ -1,48 +1,62 @@
 let navegation = document.querySelector(".letter-navegation")
-navegation.addEventListener("mouseover",()=>{
+navegation.addEventListener("mouseover", () => {
     document.querySelector(".a1").classList.add("letter")
 })
-navegation.addEventListener("mouseout",()=>{
+navegation.addEventListener("mouseout", () => {
     document.querySelector(".letter").classList.remove("letter")
 })
-navegation.addEventListener("mouseover",()=>{
+navegation.addEventListener("mouseover", () => {
     document.querySelector(".a2").classList.add("letter")
 })
-navegation.addEventListener("mouseout",()=>{
+navegation.addEventListener("mouseout", () => {
     document.querySelector(".letter").classList.remove("letter")
 })
-navegation.addEventListener("mouseover",()=>{
+navegation.addEventListener("mouseover", () => {
     document.querySelector(".a3").classList.add("letter")
 })
-navegation.addEventListener("mouseout",()=>{
+navegation.addEventListener("mouseout", () => {
     document.querySelector(".letter").classList.remove("letter")
 })
-navegation.addEventListener("mouseover",()=>{
+navegation.addEventListener("mouseover", () => {
     document.querySelector(".a4").classList.add("letter")
 })
-navegation.addEventListener("mouseout",()=>{
+navegation.addEventListener("mouseout", () => {
     document.querySelector(".letter").classList.remove("letter")
 })
 
-let selected= document.querySelector(".btn-menu")
+let selected = document.querySelector(".btn-menu")
 
-selected.addEventListener("click",()=>{
+selected.addEventListener("click", () => {
     document.querySelector(".letter-navegation").classList.toggle("selected")
 })
 
-let btnInfo1=document.querySelector(".btn-info1")
-let btnInfo2=document.querySelector(".btn-info2")
-let btnInfo3=document.querySelector(".btn-info3")
+let btnInfo1 = document.querySelector(".btn-info1")
+let btnInfo2 = document.querySelector(".btn-info2")
+let btnInfo3 = document.querySelector(".btn-info3")
+    btnInfo1.addEventListener("click", () => {
+        document.querySelector(".show-info1").classList.toggle("info1")
+    }
+    )
+    btnInfo2.addEventListener("click", () => {
+        document.querySelector(".show-info2").classList.toggle("info4")
+    }
+    )
+    btnInfo3.addEventListener("click", () => {
+        document.querySelector(".show-info3").classList.toggle("info3")
+    }
+    )
 
-btnInfo1.addEventListener("click",()=>{
-    document.querySelector(".show-info1").classList.toggle("info1")
-}
-)
-btnInfo2.addEventListener("click",()=>{
-    document.querySelector(".show-info2").classList.toggle("info4")
-}
-)
-btnInfo3.addEventListener("click",()=>{
-    document.querySelector(".show-info3").classList.toggle("info3")
-}
-)
+// else if (screen.width < 920) {
+//     btnInfo1.addEventListener("click", () => {
+//         document.querySelector(".show-info1").classList.toggle("info1")
+//     }
+//     )
+//     btnInfo2.addEventListener("click", () => {
+//         document.querySelector(".show-info2").classList.toggle("info4")
+//     }
+//     )
+//     btnInfo3.addEventListener("click", () => {
+//         document.querySelector(".show-info3").classList.toggle("info3")
+//     }
+//     )
+// }
